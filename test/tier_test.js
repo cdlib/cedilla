fc13819d0e3bc62d2cd317661c0631091a7b28d5
briley <brian.riley@ucop.edu>
2014-05-21 14:22:16 -0700

    Got tier_test.js working

--- test/tier_test.js
+++ test/tier_test.js
@@ -7,7 +7,8 @@ describe('tier.js', function(){
 	
 	var getAttributeMap = undefined,
 			processTier = undefined,
-			tiers = [];
+			tiers = [],
+			rootItem = '';
 	
 	// ---------------------------------------------------------------------------------------------------
 	before(function(done){
@@ -15,7 +16,6 @@ describe('tier.js', function(){
       var map = {};
 
       if(typeof CONFIGS['data']['objects'][type] != 'undefined'){
-        
         _.forEach(CONFIGS['data']['objects'][type]['attributes'], function(attribute){
           map[attribute] = value;
         });
@@ -30,27 +30,21 @@ describe('tier.js', function(){
       return map;
     };
 		
-		// ----------------------------------------------------------------------
-		processTier = function(socket, item, additionalServices, index, callback){
-			if(tiers[index]){
-		    console.log('Processing tier ' + tiers[index].getName());
-    
-		    tiers[index].negotiate(socket, item, function(augmentedItem, leftoverServices){
-		      // If there were leftover services and there are other tiers, add the services to the next tier
-		      if(!_.isEmpty(leftoverServices) && tiers[index + 1] instanceof Tier){
-		        tiers[index + 1].addServices(leftoverServices);
-		      }
-				
-		      return processTier(socket, augmentedItem, leftoverServices, (index + 1), callback);
-		    });
-				
-		  } else {
-		    callback();
-		  }
-		};
+		// Get the root item
+		_.forEach(CONFIGS['data']['objects'], function(def, type){
+			if(def['root'] == true){
+				rootItem = type;
+			}
+		});
 		
+		done();
+	});
+	
+	beforeEach(function(done){
 		// ----------------------------------------------------------------------
 		// Build out the tiers and their services as defined in the config
+		tiers = [];
+		
 		_.forEach(CONFIGS['services']['tiers'], function(svcs, tier){
 			var mockServices = [];
 			
@@ -89,69 +83,175 @@ describe('tier.js', function(){
 	
 	// ---------------------------------------------------------------------------------------------------
 	it('should negotiate but be unable to call any of the services due to minimum citation check', function(done){
-		var socket = new events.EventEmitter(),
-				messages = [],
-				item = undefined;
+		var _complete = 0,
+				svcsResponding = [],
+				runAlwaysCount = 0;
 		
-		// Create a socket event for each data type.
-		_.forEach(CONFIGS['data']['objects'], function(def, type){
-			if(typeof def['root'] != 'undefined'){
-				item = helper.mapToItem(type, false, {'foo':'bar'});
-			}
+	  // Setup a heartbeat monitor
+	  var heartbeat = setInterval(function(){
+			if(_complete >= _.size(tiers)){
+	      clearInterval(heartbeat);
+		
+				console.log('.... responses received from mocks for: ' + svcsResponding);
+		
+				assert.equal(runAlwaysCount, _.size(svcsResponding));
+		
+	      done();
+	    }
+	  }, 1000);
+				
+		_.forEach(tiers, function(tier){
+			var socket = new events.EventEmitter(),
+					item = undefined;
 			
-			socket.on(type, function(data){
-				console.log(data);
-				messages.push(data);
+			console.log('checking tier ' + tier.getName() + ' with an invalid item.');
+		
+			// Create a socket event for each data type.
+			_.forEach(CONFIGS['data']['objects'], function(def, type){
+				if(typeof def['root'] != 'undefined'){
+					item = helper.mapToItem(type, false, {'foo':'bar'});
+				}
+			
+				socket.on(type, function(data){
+					var json = JSON.parse(data);
+					
+					// If this is the first NON-ERROR response from the service record the service
+					if(!_.contains(svcsResponding, json['service']) && (typeof json['error'] == 'undefined')){
+						svcsResponding.push(json['service']);
+					}
+					
+				});
+			});
+		
+			socket.on('complete', function(data){
+				// Make sure the 'dispatch_always' services returned data
+				_.forEach(tier.getServiceNames(), function(service){
+					if(_.contains(CONFIGS['rules']['dispatch_always'], service)){
+						runAlwaysCount++;
+						
+						assert(_.contains(svcsResponding, tier.getServiceDisplayName(service)));
+					}
+				});
+				
+				_complete++;
+			});
+		
+			tier.negotiate(socket, item, function(augmentedItem, leftoverServices){
+				socket.emit('complete', CONFIGS['message']['broker_response_success']);
 			});
 		});
 		
-		socket.on('complete', function(data){
-			assert.equal(1, _.size(messages));
-			done();
+	});
+	
+	// ---------------------------------------------------------------------------------------------------
+	it('should negotiate and return messages from each service', function(done){
+		var _complete = 0,
+				svcsResponding = [],
+				totalSvcs = 0;
+		
+		// Count up all of the services
+		_.forEach(tiers, function(tier){
+			totalSvcs += tier.getServiceCount();
 		});
+		
+	  // Setup a heartbeat monitor
+	  var heartbeat = setInterval(function(){
+			if(_complete >= _.size(tiers)){
+	      clearInterval(heartbeat);
+				
+				console.log('.... responses received from mocks for: ' + svcsResponding);
+				
+				assert.equal(totalSvcs, _.size(svcsResponding));
+				
+	      done();
+	    }
+	  }, 1000);
+		
+		// Process each tier in order
+		_.forEach(tiers, function(tier){
+			var socket = new events.EventEmitter(),
+					item = undefined;
+		
+			console.log('checking tier ' + tier.getName() + ' with a valid item.');
+		
+			// Create a socket event for each data type.
+			_.forEach(CONFIGS['data']['objects'], function(def, type){
+				if(typeof def['root'] != 'undefined'){
+					item = helper.mapToItem(type, false, getAttributeMap(type, 'bar'));
+				}
 			
-		processTier(socket, item, [], 0, function(){
-			socket.emit('complete', CONFIGS['message']['broker_response_success']);
+				socket.on(type, function(data){
+					var json = JSON.parse(data);
+					
+					// If this is the first NON-ERROR response from the service record the service
+					if(!_.contains(svcsResponding, json['service']) && (typeof json['error'] == 'undefined')){
+						svcsResponding.push(json['service']);
+					}
+					
+				});
+			});
+		
+			socket.on('complete', function(data){
+				// Make sure the 'dispatch_always' services returned data
+				_.forEach(tier.getServiceNames(), function(service){
+					if(_.contains(CONFIGS['rules']['dispatch_always'], service)){
+						assert(_.contains(svcsResponding, tier.getServiceDisplayName(service)));
+					}
+				});
+				
+				_complete++;
+			});
+		
+			tier.negotiate(socket, item, function(augmentedItem, leftoverServices){
+				socket.emit('complete', CONFIGS['message']['broker_response_success']);
+			});
 		});
 			
 	});
 	
+	
 	// ---------------------------------------------------------------------------------------------------
-	/*it('should negotiate and return the augmented item', function(done){
-		var socket = new events.EventEmitter(),
-				messages = [],
-				item = undefined;
+	it('should return only the services that meet the minimum item attributes rules', function(done){
 		
-		// Create a socket event for each data type.
-		_.forEach(CONFIGS['data']['objects'], function(def, type){
-			if(def['root'] == true){
-				item = helper.mapToItem(type, false, getAttributeMap(type, 'yadda'));
-			}
-			
-			socket.on(type, function(data){
-//				console.log('received: ' + data + '\n\n');
-				messages.push(data);
-			});
-		});
+	});
+	
+	// ---------------------------------------------------------------------------------------------------
+	it('should correctly pass services on hold to the next tier', function(done){
 		
-		socket.on('complete', function(data){
-			console.log(_.size(messages) + ' messages received');
-			assert.equal(3, _.size(messages));
-			done();
-		});
-			
-		tiers = _.sortBy(tiers, function(item){ return item.getName() });
+	});
+	
+	// ---------------------------------------------------------------------------------------------------
+	it('should handle errors from a service correctly', function(done){
 		
-		console.log('kick off');
+		// Undefined responses
 		
-		processTier(socket, item, [], 0, function(){
-			socket.emit('complete', CONFIGS['message']['broker_response_success']);
-		});
-			
-	});*/
+		// Error messages from service
+		
+	});
 	
 });
 
+
+// ---------------------------------------------------------------------------------------------------
+Tier.prototype.getServiceDisplayName = function(serviceName){
+	var ret = '';
+	_.forEach(this._services, function(service){
+		if(service.getName() == serviceName){
+			ret = service.getDisplayName();
+		}
+	});
+	return ret;
+};
+// ---------------------------------------------------------------------------------------------------
+Tier.prototype.getServiceNames = function(){
+	var ret = [];
+	_.forEach(this._services, function(service){
+		ret.push(service.getName());
+	});
+	return ret; 
+};
+// ---------------------------------------------------------------------------------------------------
+Tier.prototype.getServices = function(){ return this._services; }
 
 // ---------------------------------------------------------------------------------------------------
 // mock the actual call to the service
@@ -195,5 +295,5 @@ Tier.prototype._callService = function(service, item, headers, callback){
 		});
 	}
 	
-	callback(obj);
+	callback([obj]);
 };
