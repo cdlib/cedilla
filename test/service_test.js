753be5ef5850aea62a2e3699e1cfb80f6010dfb8
briley <brian.riley@ucop.edu>
2014-05-22 14:01:59 -0700

    updated service to eventEmitter

--- test/service_test.js
+++ test/service_test.js
@@ -21,9 +21,9 @@ Service.prototype.setItemTypes = function(values){ this._itemTypesReturned = val
 
 				
 describe('service.js', function(){
+	this.timeout(10000);
 	
-	var svc = undefined,
-			item = undefined,
+	var item = undefined,
 			returnField = undefined,
 			returnValue = 'foo-bar',
 			mockService = undefined;
@@ -45,8 +45,6 @@ describe('service.js', function(){
 		// Spin up some stub http servers for testing
 		mockService = spinUpServer(returnField, returnValue);
 
-		svc = new Service('tester');
-		
 		done();
 	});
 	
@@ -123,21 +121,47 @@ describe('service.js', function(){
 // Calling the service
 // ---------------------------------------------------------------------------------------------------	
 	it('should throw an error when the connection to the service is refused!', function(done){
-		// Doesn't fire when target is null
-		assert.throws(function(){ svc.call(item, {}, function(result){ }); });
+		// No Target defined!
+		var _svc = new Service('tester');
 		
-		// Could Not Connect to Target
-		svc.setTarget("http://localhost:9999/fail");
-		svc.call(item, {}, function(result){
-			
-			assert(result instanceof Item);
-			
-			assert.equal(helper.buildMessage(CONFIGS['message']['service_connection_refused'], [svc.getName()]), result.getAttribute('message'));
-			
+		_svc.on('success', function(items){
+			// Force an assertion failure because this should have fired an error
+			assert.equal('', 'Should NOT have returned a success message!!!');
 			done();
 		});
+		
+		_svc.on('error', function(error){
+			assert.equal(helper.buildMessage(CONFIGS['message']['service_no_target_defined'], [_svc.getDisplayName()]), error.getAttribute('message'));
+			done();
+		});
+		
+		_svc.call(item, {});
+		
 	});
-	
+
+	// ---------------------------------------------------------------------------------------------------	
+	it('should return a valid response from the service!', function(done){
+		// Success HTTP 200
+		var _svc = new Service('tester');
+		
+		_svc.on('success', function(items){
+			assert(items instanceof Array);
+			assert(_.first(items) instanceof Item);
+			assert.equal(returnValue, _.first(items).getAttribute(returnField));
+			done();
+		});
+		
+		_svc.on('error', function(error){
+			// Force an assertion failure because this should NOT have fired an error
+			assert.equal(error.getAttribute('message'), 'Should have returned a success message!!!');
+			done();
+		});
+		
+		_svc.setTarget("http://localhost:9000/success");
+		_svc.call(item, {});
+	});
+
+/*	
 	// ---------------------------------------------------------------------------------------------------	
 	it('should return a valid response from the service!', function(done){
 		// Success HTTP 200
@@ -279,7 +303,7 @@ describe('service.js', function(){
 			
 			done();
 		});
-	});
+	});*/
 
 	
 });
@@ -324,7 +348,7 @@ function spinUpServer(returnField, returnValue){
 		// Send back the appropriate response based on the route
 		// ----------------------------------------------------------------------------------------------
 		request.on('end', function(){ 
-			response.writeHead(404);
+			//response.writeHead(404);
 			
 			var json = JSON.parse(body);
 				
