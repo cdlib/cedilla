ec19df72d086b59ad316df5d046bd21143cabed6
Joe Ferrie <jferrie@cdla-api-stg.cdlib.org>
2014-05-23 16:09:10 -0700

    began work on specializer

---
+++ test/specializer_test.js
@@ -0,0 +1,10 @@
+require("../index.js");
+
+describe('Specializer', function(){
+  describe('#specialize()', function(){
+    it('should set the ourl_version field in an item', function(){
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    })
+  })
+})
