94c0b29d264a62b5770bec97da1b3fee6140f9d2
briley <brian.riley@ucop.edu>
2014-08-20 10:46:34 -0700

    Switched from os.hostname back to hardcoded localhost in router_test

--- test/router_test.js
+++ test/router_test.js
@@ -4,69 +4,71 @@ require("./prep.js");
 describe('router.js testing', function(){
   this.timeout(10000);
   
-  var os = require('os');
-  
+	var os = require('os');
+	
   // ----------------------------------------------------------------------------------------
   before(function(done){
     // Wait for the config file and init.js have finished loading before starting up the server
     var delayStartup = setInterval(function(){
       if(typeof Item != 'undefined'){
         clearInterval(delayStartup);
-    
+		
         require('../cedilla.js');
         
         setTimeout(function(){
           console.log('.... pausing to wait for Cedilla startup.');
-          done();
-        }, 1000);
-      }
-    }, 500);
-  });
+					done();
+				}, 1000);
+			}
+		}, 500);
+	});
 
-  // ----------------------------------------------------------------------------------------
-  it('should return the index.ejs', function(done){
-    var target = 'http://' + os.hostname() + ':' + CONFIGS['application']['port'] + '/';
+	// ----------------------------------------------------------------------------------------
+	it('should return the index.ejs', function(done){
+	  var target = 'http://localhost:' + CONFIGS['application']['port'] + '/';
   
-    console.log('ROUTER: should return the index.ejs from ' + target);
+	  console.log('ROUTER: should return the index.ejs from ' + target);
   
-    sendRequest(url.parse(target), {}, function(status, headers, body){
-      assert.equal(status, 200);
-      assert(body.indexOf('Test an OpenUrl') >= 0);
+		console.log('host: ' + os.hostname());
+	
+	  sendRequest(url.parse(target), {}, function(status, headers, body){
+	    assert.equal(status, 200);
+			assert(body.indexOf('Test an OpenUrl') >= 0);
   
-      done();
-    });
-  });
+	    done();
+	  });
+	});
 
-  // ----------------------------------------------------------------------------------------
-  it('should call the citation echo service', function(done){
-    // This one gets a decent test of the OpenURL conversion as well
-    var qs = helper.mapToQueryString(helper.itemToMap(fullItem)),
-        target = 'http://' + os.hostname() + ':' + CONFIGS['application']['port'] + '/citation?' + qs;
+	// ----------------------------------------------------------------------------------------
+	it('should call the citation echo service', function(done){
+	  // This one gets a decent test of the OpenURL conversion as well
+	  var qs = helper.mapToQueryString(helper.itemToMap(fullItem)),
+	      target = 'http://localhost:' + CONFIGS['application']['port'] + '/citation?' + qs;
   
-    console.log('ROUTER: should properly echo back the openURL as a citation item (as JSON)');
+	  console.log('ROUTER: should properly echo back the openURL as a citation item (as JSON)');
   
-    var headers = {'Accept': 'text/json', 'Accept-Charset': 'utf-8'};
-    
-    sendRequest(url.parse(target), headers, function(status, headers, body){
-      assert.equal(status, 200);
-      assert.equal('application/json', headers['content-type']);
+		var headers = {'Accept': 'text/json', 'Accept-Charset': 'utf-8'};
+		
+	  sendRequest(url.parse(target), headers, function(status, headers, body){
+	    assert.equal(status, 200);
+	    assert.equal('application/json', headers['content-type']);
   
-      var json = JSON.parse(body),
-          config = CONFIGS['data']['objects'][fullItem.getType()]['attributes'];
+	    var json = JSON.parse(body),
+					config = CONFIGS['data']['objects'][fullItem.getType()]['attributes'];
   
-      _.forEach(config, function(attribute){
-        if(attribute == 'original_citation'){
-          assert.equal(qs, json['original_citation']);
+	    _.forEach(config, function(attribute){
+	      if(attribute == 'original_citation'){
+	        assert.equal(qs, json['original_citation']);
         
-        }else{
-          assert.equal('foo-bar', json[attribute]);
-        }
-      });
+	      }else{
+	        assert.equal('foo-bar', json[attribute]);
+	      }
+	    });
   
-      done();
-    });
-  });
-  
+	    done();
+	  });
+	});
+	
 });
 
 // ----------------------------------------------------------------------------------------
@@ -78,9 +80,9 @@ var sendRequest = function(target, headers, callback){
                    path: target.path,
                    method: 'GET'};
 
-  if(_.size(headers) > 0){
-    _options['headers'] = headers;
-  }
+	if(_.size(headers) > 0){
+		_options['headers'] = headers;
+	}
 
   try{
     var _request = _http.request(_options, function(response){
@@ -96,11 +98,11 @@ var sendRequest = function(target, headers, callback){
       });
     });
     
-    _request.on('error', function(err){
-      console.log(err.message);
-      console.log(err.stack);
-    });
-    
+		_request.on('error', function(err){
+			console.log(err.message);
+			console.log(err.stack);
+		});
+		
     _request.end();
   
   }catch(Error){
