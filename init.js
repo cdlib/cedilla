c9834fb7efcc0be77006987979f48b4ea520ca15
jferrie <joe.ferrie@ucop.edu>
2014-12-17 10:56:12 -0800

    more intuitive (for me) module export

--- init.js
+++ init.js
@@ -1,23 +1,22 @@
-// Load up all of our modules once
+// Load up all of our modules once and re-export
 
 // These modules are used throughout (Broker, Tier, Service, etc.)
-module.exports = fs = require('fs');
-module.exports = yaml = require('js-yaml');
-module.exports = _ = require('underscore');
-module.exports = util = require('util');  
-module.exports = events = require('events');
+exports.fs = fs = require('fs');
+exports.yaml = yaml = require('js-yaml');
+exports._ = _ = require('underscore');
+exports.util = util = require('util');  
+exports.events = events = require('events');
+exports.CONFIGS = CONFIGS = require("./lib/config.js");
 
-module.exports = CONFIGS = require("./lib/config.js");
-
-// Used by Tier
-module.exports = Encoder = require('node-html-encoder').Encoder;
+// Used by Tier (not currently)
+// module.exports = Encoder = require('node-html-encoder').Encoder;
 
 // Used by Service
-module.exports = url = require('url');
-module.exports = uuid = require('node-uuid');
+exports.url = url = require('url');
+exports.uuid = uuid = require('node-uuid');
 
 // Used in helper
-module.exports = querystring = require('querystring');
+exports.querystring = querystring = require('querystring');
 
 var i = 0;
 
@@ -26,30 +25,28 @@ var waitForConfigs = setInterval(function(){
   if(typeof CONFIGS['application'] != 'undefined' || i == 2000){
     clearInterval(waitForConfigs);
  
-    module.exports = helper = require("./lib/utils/helper.js");
-    module.exports = serializer = require("./lib/utils/serializer.js");
-    module.exports = Translator = require("./lib/utils/translator.js");
-    module.exports = Consortial = require("./lib/utils/consortial.js");
-    
-    module.exports = Request = require("./lib/models/request.js");
-		module.exports = Requestor = require("./lib/models/requestor.js");
-    module.exports = Item = require("./lib/models/item.js");
+    exports.helper = helper = require("./lib/utils/helper.js");
+    exports.serializer = serializer = require("./lib/utils/serializer.js");
+    exports.Translator = Translator = require("./lib/utils/translator.js");
+    exports.Consortial = Consortial = require("./lib/utils/consortial.js");
+    exports.Request = Request = require("./lib/models/request.js");
+    exports.Requestor = Requestor = require("./lib/models/requestor.js");
+    exports.Item = Item = require("./lib/models/item.js");
 
     //module.exports = LOGGER = require('./lib/logger.js');
-    module.exports = Broker = require("./lib/broker.js");
-    module.exports = Tier = require("./lib/tier.js");
-    module.exports = Service = require("./lib/service.js");
-		
-		module.exports = OpenUrlParser = require("./lib/parsers/openurl.js");
+    exports.Broker = Broker = require("./lib/broker.js");
+    exports.Tier = Tier = require("./lib/tier.js");
+    exports.Service = Service = require("./lib/service.js");
+    exports.OpenUrlParser = OpenUrlParser = require("./lib/parsers/openurl.js");
 
-		// Setup logger
-		module.exports = log = require('./lib/logger.js');
+    // Setup logger
+    exports.log = log = require('./lib/logger.js');
 
-		//Load registered notifiers
-		module.exports = notifiers = registerNotifiers();
+    //Load registered notifiers
+    exports.notifiers = notifiers = registerNotifiers();
 
     // Should be for TEST only!
-    module.exports = assert = require("assert");
+    exports.assert = assert = require("assert");
   }
   i++;
 }, 200);
