6f4d437752b93450cd2a86ab1c9d1e1d00b24eec
jferrie <joe.ferrie@ucop.edu>
2015-08-06 12:38:49 -0700

    performance testing

--- test/bench/performance/request_list.rb
+++ test/bench/performance/request_list.rb
@@ -3,11 +3,15 @@
 # Two arguments: input log file, number of lines
 
 i = 0
-query_regex = /"GET\s\/sfx_local(.+)\sHTTP/
+query_regex = /"GET\s\/sfx_local\??&?(url_ver=Z39.88.+)\sHTTP/
 log = ARGV[0]
 f = File.open(log, 'r')
-f.each_line do |l|
+f.each do |l|
   i += 1
-  puts l.match(query_regex).captures
+  unless l.match(query_regex).nil?
+    unless l.match(/pmid/) || l.match(/doi/)
+      puts l.match(query_regex).captures
+    end
+  end
   break if i > ARGV[1].to_i
 end
--- test/bench/performance/sfx/ab_test.rb
+++ test/bench/performance/sfx/ab_test.rb
@@ -3,12 +3,13 @@
 # Two arguments: input url file, number of lines
 
 i = 0
+j = 0
 TOTAL_REGX = /^Total:\s+(\d+)\s+/
 queries_filename = ARGV[0]
 limit = ARGV[1].to_i
 queries = File.open(queries_filename, 'r')
-TARGET_PREFIX = 'http://cdla-dev.cdlib.org:3005/'
-#TARGET_PREFIX = 'http://ucelinks.cdlib.org:8888/sfx_test'
+#TARGET_PREFIX = 'http://cdla-dev.cdlib.org:3005/'
+TARGET_PREFIX = 'http://ucelinks.cdlib.org:8888/sfx_test'
 BASE_QUERY = '?sfx.response_type=multi_obj_detailed_xml&sfx.show_availability=1&'
 outfiles = []
 totals = []
@@ -17,10 +18,16 @@ queries.each do |l|
   query = l.chomp
   i += 1
   puts "#{TARGET_PREFIX}#{BASE_QUERY}#{query}"
-  `ab -c 1 -n 1 -S -g abresults/fileout#{i} "#{TARGET_PREFIX}#{BASE_QUERY}#{query}" > abresults/streamout#{i}`   
-  outfiles << "abresults/streamout#{i}"
+  # call each three times
+  # throw out the first result to warm up any cache
+  for j in 0..2 do
+    `ab -c 1 -n 1 -S -g abresults/fileout#{i} "#{TARGET_PREFIX}#{BASE_QUERY}#{query}" > abresults/streamout#{i}`
+    if j > 0
+      outfiles << "abresults/streamout#{i}"
+    end
+    sleep 5
+  end
   break if i >= limit && limit >= 1
-  sleep 5
 end
 
 outfiles.each do |file|
