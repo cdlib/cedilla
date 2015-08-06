de37474ed0132bddb6638350441c0ce37ffc3134
jferrie <joe.ferrie@ucop.edu>
2015-08-06 04:29:31 -0700

    performance test summary

--- test/bench/performance/cedilla/summarize.rb
+++ test/bench/performance/cedilla/summarize.rb
@@ -2,13 +2,41 @@
 # Summarizes the results of the test
 
 results = File.open('wb_result.csv', 'r')
+filtered = File.open('filtered_result.csv', 'w+')
+i = 0
+
+# For each query there are three results
+# Filter the results to move the first one
+# in each series
+# for the purpose of warming the cache
+# and getting a more uniform result
+results.each do |l|
+ unless i == 0 || i % 3 == 0
+  filtered << l
+ end
+ i += 1
+end
+
 resource_times = []
 complete_times = []
-results.each do |l|
-  both = l.split(',')
-  resource_times << both[0].to_i
-  complete_times << both[1].to_i
+
+
+# the last value in each row will be the complete time
+# if there is only one value there are no resources
+rows = []
+filtered.seek(0)
+filtered.each do |l|
+  rows << l
+  row = l.split(',')
+  if row.size > 1
+    resource_times << row[0].to_i 
+  end
+  complete_times << row[-1].to_i
 end
+
+# output all the results
+puts "Total rows: #{rows.size}"
+
 puts "\nResource times:"
 puts "Max: #{resource_times.max}"
 puts "Min: #{resource_times.min}"
--- test/bench/performance/cedilla/wb_test.sh
+++ test/bench/performance/cedilla/wb_test.sh
@@ -2,29 +2,33 @@
 # Runs a single websocket-bench test in the background
 
 filename=$1
+mv wb_result.csv wbresult.csv.old
+touch wb_result.csv
 
 function wb {
   concurrency="1"
   total="1"
   workers="1"
-  websocket-bench -a $total -c $concurrency -w $workers -g cdla_generator.js  -t socket.io -p socket.io -k http://d2d-cdla-dev.cdlib.org:3005 &
-  proc=$!
-  # give time for all processes to initialize
-  sleep 1
-  # get child process of prod
-  worker=$(pgrep -P $proc)
+
+  for i in {0..2}; do
+    websocket-bench -a $total -c $concurrency -w $workers -g cdla_generator.js  -t socket.io -p socket.io -k http://d2d-cdla-dev.cdlib.org:3005 &
+    proc=$!
+    # give time for all processes to initialize
+    sleep 1
+    # get child process of prod
+    worker=$(pgrep -P $proc)
 
   # kill them now? or later? (Pirate Jenny)
-  sleep 5
-  kill -9 $worker
-  kill -9 $proc
+    sleep 10
+    kill -9 $worker
+    kill -9 $proc
+  done
 }
 
 outfile="~qry"
 while read -r line
 do
     name=$line
-    echo "loop"
     echo $name > $outfile
     wb 
 done < "$filename"
